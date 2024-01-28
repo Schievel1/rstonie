ebf69a144916b0bc3e62039afd1639b72f790255
Pascal Jäger <pascal.jaeger@leimstift.de>
2024-01-28 10:29:35 +0100

    add some nicer logging info

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,11 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    // Open the media source.
     let src = std::fs::File::open(&args.input)?;
+
     // Create the media source stream.
     let mss = MediaSourceStream::new(Box::new(src), Default::default());
+
     // if the input file has an extension, use it as a hint for the media format.
     let mut hint = Hint::new();
     if let Some(ext) = args.input.extension() {
@@ -38,56 +39,66 @@ fn main() -> Result<()> {
             hint.with_extension(ext);
         }
     }
+
     let meta_opts: MetadataOptions = Default::default();
     let fmt_opts: FormatOptions = Default::default();
+
     // Probe the media source.
     let probed = symphonia::default::get_probe().format(&hint, mss, &fmt_opts, &meta_opts)?;
+
     // Get the instantiated format reader.
     let mut format = probed.format;
-    // Find the first audio track with a known (decodeable) codec.
     let track = format
         .tracks()
         .iter()
         .find(|t| t.codec_params.codec != CODEC_TYPE_NULL)
         .ok_or(anyhow::anyhow!("No supported audio track found"))?;
-    // Use the default options for the decoder.
-    let dec_opts: DecoderOptions = Default::default();
+
     // Create a decoder for the track.
+    let dec_opts: DecoderOptions = Default::default();
     let mut decoder = symphonia::default::get_codecs().make(&track.codec_params, &dec_opts)?;
+
     // Store the track identifier, it will be used to filter packets.
     let track_id = track.id;
 
+    // create a Toniefile to write to later
     let mut rnd = rand::thread_rng();
     let audio_id = rnd.gen::<u32>();
     let file = File::create(args.output)?;
-    // let strcomments: Option<Vec<&str>>;
-    // if let Some(comments) = args.comments {
-    //     // let c = comments.iter().map(AsRef::as_ref).collect();
-    //     strcomments = Some(comments.iter().map(AsRef::as_ref).collect());
-    // } else {
-    //     strcomments = None;
-    // }
-    let mut toniefile = Toniefile::new(file, audio_id, None)?;
-    let input_sample_rate = track.codec_params.sample_rate.unwrap();
-    let input_channels = track.codec_params.channels.unwrap().count(); //TODO
-    // let input_bitrate = track.codec_params.bits_per_sample.unwrap();
+    let comments = args.comments.unwrap_or_default();
+    let strcomments: Option<Vec<&str>> = if comments.is_empty() {
+        None
+    } else {
+        Some(comments.iter().map(AsRef::as_ref).collect())
+    };
+    let mut toniefile = Toniefile::new(file, audio_id, strcomments)?;
+
+    // create a resampler to convert to 48kHz
+    let mut resampler: Option<Resampler<i16>> = None;
+
+    let input_sample_rate = track.codec_params.sample_rate.unwrap_or_default();
+    let input_channels = track.codec_params.channels.unwrap_or_default().count(); //TODO
+    let tracklen = track.codec_params.n_frames.unwrap_or_default();
+
+    // print some file info
     println!(
         "Input file: {} Hz, {} channels",
-        input_sample_rate,
-        input_channels,
+        input_sample_rate, input_channels,
     );
 
-    // let signalspec = SignalSpec::new(track.codec_params.sample_rate.unwrap(), track.codec_params.channels.unwrap());
+    if !comments.is_empty() {
+        for comment in comments {
+            println!("ogg user comment: {}", comment);
+        }
+    }
+    println!("Track length: {} frames", tracklen);
 
-    let mut resampler: Option<Resampler<i16>> = None;
-    // The decode loop.
     while let Ok(packet) = format.next_packet() {
+        let progress = packet.ts * 100 / tracklen;
+        print!("\rProgress: {}%", progress);
         // Consume any new metadata that has been read since the last packet.
         while !format.metadata().is_latest() {
-            // Pop the old head of the metadata queue.
             format.metadata().pop();
-
-            // Consume the new metadata at the head of the metadata queue.
         }
 
         // If the packet does not belong to the selected track, skip over it.
@@ -106,14 +117,9 @@ fn main() -> Result<()> {
                         decoded.capacity() as u64 / 2,
                     ));
                 }
-                // let resampled = resampler.resample(decoded.clone()).unwrap();
                 if let Some(resampled) = resampler.as_mut().unwrap().resample(decoded.clone()) {
-                    toniefile
-                        .encode(resampled)?;
+                    toniefile.encode(resampled)?;
                 }
-                // let mut sample_buf =
-                    // SampleBuffer::<i16>::new(decoded.capacity() as u64, *decoded.spec());
-                // sample_buf.copy_interleaved_ref(decoded);
             }
             Err(Error::IoError(_)) => {
                 // The packet failed to decode due to an IO error, skip the packet.
@@ -132,7 +138,7 @@ fn main() -> Result<()> {
         }
     }
     toniefile.finalize()?;
+    println!("\rProgress: 100%");
+    println!("Done");
     Ok(())
 }
-
-
